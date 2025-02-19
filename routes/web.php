a25e8ea01d853b291449f91b65143db09e1fcea3
Naditya206 <aditandino2019211@gmail.com>
2025-02-19 10:33:45 +0700

    J2

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/{category}', [ProductController::class, 'category']);
+});
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+Route::get('/sales', [SalesController::class, 'index']);
+
