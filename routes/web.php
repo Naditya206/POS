41b38ea509fd042e7c5fca75d40513764c1f919f
Naditya206 <aditandino2019211@gmail.com>
2025-03-11 08:04:25 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriControllerController;
+use App\Http\Controllers\WelcomeController;
+
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -54,5 +56,5 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
+Route::get('/', [WelcomeController::class, 'index']);
 
